f3eb384f7807e02f1d90a8987b4133d84209e1dd
Desire <desireirankundawilliams@gmail.com>
2022-10-11 15:27:00 +0200

    created routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import Header from './Component/Header';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Component/Header";
+import Home from "./Component/Home";
+import Cart from "./Component/Cart";
 
 function App() {
   return (
     <div className="App">
-    <Header />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
